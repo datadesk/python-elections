c0237797e824c5de36b0403758adb885eb3621c3
palewire <ben.welsh@gmail.com>
2013-10-23 07:09:30 -0700

    Upped version

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
         file_info[0] = '\\PURELIB\\%s' % file_info[0]
 
 setup(name='python-elections',
-      version='0.40',
+      version='0.41',
       description='A Python wrapper for the Associated Press\'s U.S. election data service.',
       author='The Los Angeles Times Data Desk',
       author_email='datadesk@latimes.com',
