1efb17f41de7743cafbea5ced45b85af2e5424b5
palewire <ben.welsh@gmail.com>
2014-06-09 11:00:53 -0700

    Leapt forward to 1.0.0 strictly to get on a semantic versioning scheme for the future. Fixes #89.

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'bdist_wininst':
         file_info[0] = '\\PURELIB\\%s' % file_info[0]
 
 setup(name='python-elections',
-      version='0.41',
+      version='1.0.0',
       description='A Python wrapper for the Associated Press\'s U.S. election data service.',
       author='The Los Angeles Times Data Desk',
       author_email='datadesk@latimes.com',
