6833b5179dd0f7a571a8436ae05e9ce4e3bc1d70
palewire <ben.welsh@gmail.com>
2014-11-20 22:09:24 -0800

    Classifiers update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='python-elections',
-    version='1.1.1',
+    version='1.1.2',
     description="A Python wrapper for the AP's U.S. election data service.",
     author='The Los Angeles Times Data Desk',
     author_email='datadesk@latimes.com',
@@ -14,7 +14,7 @@ setup(
         'python-dateutil==1.5',
         'BeautifulSoup==3.2.1',
     ],
-    classifers=(
+    classifiers=(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
