135f97aea34f6ecc1ce1a0482462581b4f48c6db
palewire <ben.welsh@gmail.com>
2014-11-20 22:06:27 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(
     name='python-elections',
-    version='1.1.0',
-    description='A Python wrapper for the Associated Press\'s U.S. election data service.',
+    version='1.1.1',
+    description="A Python wrapper for the AP's U.S. election data service.",
     author='The Los Angeles Times Data Desk',
     author_email='datadesk@latimes.com',
     url='http://www.github.com/datadesk/python-elections/',
@@ -14,4 +14,9 @@ setup(
         'python-dateutil==1.5',
         'BeautifulSoup==3.2.1',
     ],
+    classifers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    )
 )
